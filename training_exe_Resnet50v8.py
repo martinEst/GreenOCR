b8af42ba480a70f309e95dc87f649fe4c468af5a
Martin Brehhov <brehhov@yahoo.com>
2025-08-22 00:12:42 +0100

    including space

--- training_exe_Resnet50v8.py
+++ training_exe_Resnet50v8.py
@@ -125,24 +125,10 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #some special characters that appeared in germanic manuscripts, maybe should be avoided now
 ctc_loss_fn = torch.nn.CTCLoss(blank=0, reduction='mean', zero_infinity=True)
 
-vocab = [
-    # Lowercase
-    'a','b','c','d','e','f','g','h','i','j','k','l','m',
-    'n','o','p','q','r','s','t','u','v','w','x','y','z',
-
-    # Uppercase
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-
-    # Digits
-    '0','1','2','3','4','5','6','7','8','9',
-
-    # Punctuation & symbols
-    '!', '"', '#', '$', '%', '&', "'", '(', ')', '*',
-    '+', ',', '-', '.', '/', ':', ';', '<', '=', '>',
-    '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|',
-    '}', '~'
-]
+vocab = list("abcdefghijklmnopqrstuvwxyz") + \
+        list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") + \
+        list("0123456789") + \
+        list("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")  # include space
 
 
 
